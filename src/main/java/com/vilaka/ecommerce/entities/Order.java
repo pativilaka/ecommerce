e9d130e301f98b0edaf03577e67d2c679f82bc47
pativilaka <p.vilaka@gmail.com>
2023-08-03 15:45:01 -0300

    Add relacionamento ManyToMany com classe de associação

--- src/main/java/com/vilaka/ecommerce/entities/Order.java
+++ src/main/java/com/vilaka/ecommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.vilaka.ecommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -22,6 +25,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public  Order(){}
 
@@ -70,6 +76,17 @@ public class Order {
     }
 
     public void setPayment(Payment payment) {
+
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    //converto cada x OrderItem para um x. getProduct >> e pego só o produto associado a ele.
+    //Construo uma nova lista de Products e não mais de OrderItem
+    public List<Product> getProducts(){
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 }
